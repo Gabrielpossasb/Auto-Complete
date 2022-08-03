255cab46befb2b08f79d3b95c13ed9922dc5c5e8
Gabrielpossasb <gabrielpossasb@gmail.com>
2022-08-03 15:13:39 -0700

    Updates

---
+++ precache-manifest.45c7f9ccc73ad15db91af006c98f24cb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "bab1f368ac055988af5f",
+    "url": "/Auto-Complete/static/js/main.bab1f368.chunk.js"
+  },
+  {
+    "revision": "59581db7cbe71cf77f10",
+    "url": "/Auto-Complete/static/js/1.59581db7.chunk.js"
+  },
+  {
+    "revision": "2693f8876b1afd18779a",
+    "url": "/Auto-Complete/static/js/runtime~main.2693f887.js"
+  },
+  {
+    "revision": "f50ddaa965e9aa6394f2b539a9a12768",
+    "url": "/Auto-Complete/index.html"
+  }
+];
